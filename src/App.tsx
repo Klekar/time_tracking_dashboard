1e10a622539accb9d6bd0bafc1e7980ffc747745
Karel Klečka <kklecka@seznam.cz>
2023-08-21 05:00:28 +0200

    move invisible h1 to <main>

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function App() {
             <GlobalStyle />
             <StyledApp>
                 <main>
+                    <h1 style={{ display: "none" }}>Time Tracking Dashboard</h1>
                     <AcivityDashboard />
                 </main>
                 <Attribution>
