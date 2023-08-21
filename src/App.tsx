6f4f065bd486bb17eba646974ffab1235ec7a79c
Karel Klečka <kklecka@seznam.cz>
2023-08-21 04:30:51 +0200

    ccu: remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import AcivityDashboard from './components/AcivityDashboard';
 import { ThemeProvider, styled } from 'styled-components';
 import { myTheme } from './styledComponents/myTheme';
