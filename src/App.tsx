1f1fd48932623c212c1e4f7e9982c26d6e44188c
Karel Klečka <kklecka@seznam.cz>
2023-08-21 02:11:43 +0200

    add attribution

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,40 @@ import { ThemeProvider, styled } from 'styled-components';
 import { myTheme } from './styledComponents/myTheme';
 
 const StyledApp = styled.div`
-  background-color: hsl(226, 43%, 10%);
-  min-height: 100svh;
-  min-height: 100dvh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    background-color: hsl(226, 43%, 10%);
+    min-height: 100svh;
+    min-height: 100dvh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`
+
+const Attribution = styled.footer`
+    position: absolute;
+    bottom: 0;
+    color: white;
+    font-size: 0.8rem;
+
+    a {
+        color: ${props => props.theme.colors.desaturated}
+    }
 `
 
 function App() {
-  return (
-    <ThemeProvider theme={myTheme}>
-      <StyledApp>
-        <header>
-
-        </header>
-        <main>
-          <AcivityDashboard />
-        </main>
-      </StyledApp>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={myTheme}>
+            <StyledApp>
+                <main>
+                    <AcivityDashboard />
+                </main>
+                <Attribution>
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+                    Coded by <a href="https://www.frontendmentor.io/profile/Klekar" target="_blank" rel="noreferrer">Karel Klečka</a>.
+                </Attribution>
+            </StyledApp>
+        </ThemeProvider >
+    );
 }
 
 export default App;
